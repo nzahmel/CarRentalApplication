2a101f23db57cd5a4bc557679b35f29865773729
nyQ7 <nico.zahmel@gmx.de>
2020-01-03 20:36:23 +0100

    Einbindung von Image und Optimierungen

--- AutoVermietungsSoftware/src/system/MainWindow.java
+++ AutoVermietungsSoftware/src/system/MainWindow.java
@@ -11,6 +11,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -21,7 +22,9 @@ import javax.swing.border.Border;
 /**
  *
  * @author Nico Zahmel <Enjoy the flight>
- * @version 1.0
+ * @version 1.0 image source: <a href=
+ *          "https://www.freepik.com/free-photos-vectors/background">Background
+ *          vector created by rawpixel.com - www.freepik.com</a>
  */
 
 public class MainWindow extends JFrame {
@@ -31,16 +34,13 @@ public class MainWindow extends JFrame {
 	JLabel endText;
 	JButton bFahrzeugverwaltung, bKundenverwaltung, bVerfuegbarkeit;
 
-	
 	public MainWindow() {
 
 		/*
 		 *
-		 * Initialisierung & Anpassung
-		 *
+		 * Initialisierung & Anpassung Frame & Header
 		 */
-		
-		setVisible(true);
+
 		setLayout(new BorderLayout());
 		setTitle("Car Rental System for S.E. at THB");
 		setSize(800, 600);
@@ -65,7 +65,7 @@ public class MainWindow extends JFrame {
 		// Das Zentrum des Geschehens - fügt den Text hinzu & platziert ihn oben.
 		add(startText, BorderLayout.NORTH); // fügt das Text-Label oben hinzu.
 
-		// Die Mitte 'center' bekommt ein Panel mit eigenem GridLayout
+		// Die Mitte 'center' bekommt ein Panel für die Buttons
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(null);
 		add(buttonPanel);
@@ -86,24 +86,55 @@ public class MainWindow extends JFrame {
 		bKundenverwaltung.setFont(schriftartButtons);
 		bVerfuegbarkeit.setFont(schriftartButtons);
 
-		bKundenverwaltung.setBounds(275, 70, 250, 100);
-		bFahrzeugverwaltung.setBounds(275, 200, 250, 100);
-		bVerfuegbarkeit.setBounds(275, 330, 250, 100);
+		bKundenverwaltung.setBounds(50, 70, 250, 100);
+		bFahrzeugverwaltung.setBounds(50, 200, 250, 100);
+		bVerfuegbarkeit.setBounds(50, 330, 250, 100);
 
 		buttonPanel.add(bFahrzeugverwaltung);
 		buttonPanel.add(bKundenverwaltung);
 		buttonPanel.add(bVerfuegbarkeit);
 
-		// Handler verleiht den Buttons seine Funktionalität.
-		DerHandler handler = new DerHandler();
-		// Bevor Klasse DerHandler erstellt und definiert wird,
-		// ActionListener hinzufügen & handler als Objekt übergeben.
-		bFahrzeugverwaltung.addActionListener(handler); // ActionsListener passt auf, ob etwas passiert
-		bKundenverwaltung.addActionListener(handler); // & weiß, was dann zu tun ist.
-		bFahrzeugverwaltung.addActionListener(handler); // ActionsListener passt auf, ob etwas passiert
-		bVerfuegbarkeit.addActionListener(handler);
+		/**
+		 * Einbindung vom Image
+		 */
 
-		// Initialisierung & Anpassung Größe - Footer Text
+		JLabel jl = new JLabel();
+		jl.setIcon(new ImageIcon("image/autoklein.png"));
+		jl.setBounds(350, 100, 400, 300);
+		buttonPanel.add(jl);
+		validate();
+
+		/**
+		 * Handler komplett ersetzt durch Lambdas - steht hier nurnoch für bessere
+		 * Nachvollziehbarkeit. Lösung inline-version direkt unter dem auskommentiertem
+		 * Bereich:
+		 */
+
+		// // Handler verleiht den Buttons seine Funktionalität.
+		// DerHandler handler = new DerHandler();
+		// // Bevor Klasse DerHandler erstellt und definiert wird,
+		// // ActionListener hinzufügen & handler als Objekt übergeben.
+		// bFahrzeugverwaltung.addActionListener(handler); // ActionsListener passt auf,
+		// ob etwas passiert
+		// bKundenverwaltung.addActionListener(handler); // & weiß, was dann zu tun ist.
+		// bVerfuegbarkeit.addActionListener(handler);
+
+		// Lambda Version für Button-Funktionalität.
+		bKundenverwaltung.addActionListener(e -> {
+			JOptionPane.showMessageDialog(null, "Willkommen in der Fahrzeugverwaltung!");
+		});
+		bFahrzeugverwaltung.addActionListener(e -> {
+			JOptionPane.showMessageDialog(null, "Willkommen in der Kundenverwaltung!");
+		});
+		bVerfuegbarkeit.addActionListener(e -> {
+			JOptionPane.showMessageDialog(null, "Willkommen im Verfügbarkeits Check!");
+		});
+
+		
+		/*
+		 * Initialisierung & Anpassung Größe - Footer Text
+		 */
+		
 		endText = new JLabel(
 				"Created by: Nico Zahmel, Christopher Alb, " + "Wilhelm Wöhlte, Mirko Reefschläger, Torben Hammes");
 		// endText.setHorizontalAlignment(JLabel.RIGHT);
@@ -132,22 +163,27 @@ public class MainWindow extends JFrame {
 		// }
 		// });
 		// timer.start();
+
+		setVisible(true); // muss hinter allen adds gesetzt werden, ansonsten treten Probleme auf.
 	}
 
-	
-	private class DerHandler implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent event) {
-			if (event.getSource() == bFahrzeugverwaltung) {
-				// null positioniert die Meldung in der Mitte
-				// danach folgt der Ausgabe-String
-				JOptionPane.showMessageDialog(null, "Willkommen in der Fahrzeugverwaltung");
-			} else if (event.getSource() == bKundenverwaltung) {
-				JOptionPane.showMessageDialog(null, "Willkommen in der Kundenverwaltung");
-			} else if (event.getSource() == bVerfuegbarkeit) {
-				JOptionPane.showMessageDialog(null, "Willkommen im Verfügbarkeits Check");
-			}
-		}
-	}
+	/**
+	 * Komplett ersetzt durch Lambdas - steht hier nurnoch für bessere
+	 * Nachvollziehbarkeit. Lambda-Version weiter oben.
+	 */
+	// private class DerHandler implements ActionListener {
+	//
+	// @Override
+	// public void actionPerformed(ActionEvent event) {
+	// if (event.getSource() == bFahrzeugverwaltung) {
+	// // null positioniert die Meldung in der Mitte
+	// // danach folgt der Ausgabe-String
+	// JOptionPane.showMessageDialog(null, "Willkommen in der Fahrzeugverwaltung");
+	// } else if (event.getSource() == bKundenverwaltung) {
+	// JOptionPane.showMessageDialog(null, "Willkommen in der Kundenverwaltung");
+	// } else if (event.getSource() == bVerfuegbarkeit) {
+	// JOptionPane.showMessageDialog(null, "Willkommen im Verfügbarkeits Check");
+	// }
+	// }
+	// }
 }
